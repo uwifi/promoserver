838ef322be5c60e07623192a7619283777b5374e
Liu.Hanru <isuekey@github.com>
2017-10-06 14:29:50 +0800

    info

--- api/models/fund.model.js
+++ api/models/fund.model.js
@@ -29,20 +29,10 @@ function receiveEthFund(fundData){
             txHash: ele.txHash,
             fromAddress: ele.txFrom,
             toAddress: ele.txTo,
-            amountInWei: ele.txValue
+            amountInWei: ele.txValue,
+            amountHuman: ele.txHuman
         };
     })).then((instanceArray)=>{
-        instanceArray.map((ele)=> ele.toJSON()).map((ele)=>{
-            return {
-                txHash: ele.txHash,
-                status: "ok",
-                usage:"promoserver"
-            };
-        });
-        return sequelize.query("update t_trade as tt set account = (select account from t_bank where bank_address = tt.bank_address), amount_human = (amount_in_wei / 1e18);", {
-            type:Sequelize.QueryTypes.UPDATE
-        });
-    }).then((tradeUpdated)=>{
         return sequelize.query("update t_bank as tt set remain = ( select sum(amount_human) as remain from t_trade where tt.bank_address = bank_address );", {
             type:Sequelize.QueryTypes.UPDATE
         });
