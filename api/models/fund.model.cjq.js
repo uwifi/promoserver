173b39bcd4f77e806854d8989b169fddb7d189f7
ylkjcjq <691424873@qq.com>
2017-10-18 20:43:31 +0800

    update

--- api/models/fund.model.cjq.js
+++ api/models/fund.model.cjq.js
@@ -20,26 +20,33 @@ ModelFund.receiveListenerOfFund = function receiveListenerOfFund(fundInfo){
 };
 
 function receiveBitFund(fundData,bankType){
-
-    console.log('\n------------------receive data:'+JSON.stringify(fundData)+'------------------');
-    let rez = [];
-    fundData.forEach(
-        function(ele){  
-            var date = {
-                bankType:bankType,
-                bankAddress: ele.txTo,
-                txHash: ele.txHash,
-                fromAddress: ele.txFrom,
-                toAddress: ele.txTo,
-                amountInWei: ele.txValue,
-                amountHuman: ele.txHuman
-            };
-            DomainTrade.insertOrUpdate(date);
-        }
-    );
-    return sequelize.query("update t_bank as tt set remain = ( select sum(amount_human) as remain from t_trade where tt.bank_address = bank_address );", {
-            type:Sequelize.QueryTypes.UPDATE
-        }).then(()=>{
+    
+    return new Promise((resolve, reject)=>{
+        console.log('\n------------------start update trade:'+'------------------');
+        var rez = [];
+        fundData.forEach(
+            function(ele){  
+                var date = {
+                    bankType:bankType,
+                    bankAddress: ele.txTo,
+                    txHash: ele.txHash,
+                    fromAddress: ele.txFrom,
+                    toAddress: ele.txTo,
+                    amountInWei: ele.txValue,
+                    amountHuman: ele.txHuman
+                };
+                console.log('\n------------------Trade data:'+JSON.stringify(date)+'------------------\n');
+                DomainTrade.insertOrUpdate(date);
+                rez[rez.length] = date;
+            }
+        );
+        console.log('\n------------------end update trade:'+'------------------\n');
+        resolve(rez);
+    }).then((rez)=>{
+        sequelize.query("update t_bank as tt set remain = ( select sum(amount_human) as remain from t_trade where tt.bank_address = bank_address );", {
+            type:Sequelize.QueryTypes.UPDATE})
+        return rez;
+    }).then((rez)=>{
         return rez;
     });
 };
